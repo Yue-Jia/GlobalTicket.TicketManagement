ca5c8a8c6ae9e1fb55deea1fa5183f5fa574ce87
yue jia <yuejia070@gmail.com>
2021-04-24 20:49:11 -0400

    first commit

---
+++ GlobalTicket.TicketManagement.Domain/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿using GlobalTicket.TicketManagement.Domain.Common;
+using System;
+
+namespace GlobalTicket.TicketManagement.Domain.Entities
+{
+    public class Order: AuditableEntity
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public bool OrderPaid { get; set; }
+    }
+}
